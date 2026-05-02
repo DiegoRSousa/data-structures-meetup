e3884f649877e29e289b1c015366b9d6be42db75
Diego <diegors90@gmail.com>
2026-05-02 04:08:23 +0000

    adiciona settings.json

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -8,17 +8,5 @@ void main() {
 
 int[] execute(int[] arr, int target) {
 
-    var map = new HashMap<Integer, Integer>();
-
-    for(var i = 0; i < arr.length; i++) {
-        var value = target - arr[i];
-
-        if(map.containsKey(value))
-            return new int[] { map.get(value), i};
-
-        map.put(arr[i], i);
-
-    }
-
     return new int[] { -1, -1};
 }
